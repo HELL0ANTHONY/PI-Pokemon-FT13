4ca1de89ca0b6b4cc8330a8ee609f1e16e75c0e4
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-06-10 11:39:16 -0300

    Checkbox component

---
+++ client/src/components/checkbox/Checkbox.js
@@ -0,0 +1,24 @@
+import "./checkbox.css";
+
+const Checkbox = ({ label, value, name, onChange }) => {
+  return (
+    <div className="checkbox">
+      <label className="checkbox-label">
+        <div className="checkbox-btn-container">
+          <input
+            className="checkbox-input"
+            onChange={onChange}
+            type="checkbox"
+            value={value}
+            name={name}
+          />
+        </div>
+        <div className="checkbox-text">
+          {label}
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
