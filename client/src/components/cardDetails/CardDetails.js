44785ec3e7810a39a74047ac71a17f2a804d310a
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-06-11 16:48:00 -0300

    cardDetails

---
+++ client/src/components/cardDetails/CardDetails.js
@@ -0,0 +1,66 @@
+import "./cardDetails.css";
+
+const CardDetails = ({ id, name, image, height, weight, types, hp, speed, force, defense }) => {
+  const pokemonTypes = types.map(({ name }) => name).join(", ");
+    return (
+          <>
+            <div className="detail--card">
+              <div className="left--side-container">
+                <div className="left--side-detail">
+                  <div
+                    style={{ backgroundImage: `url(${image})` }}
+                    className="img-container"
+                  />
+                  <div className="card--title">
+                    <h1>{name}</h1>
+                    <h3>ID: {id}</h3>
+                  </div>
+                </div>
+              </div>
+              <div className="right-side-detail">
+                <div className="info-right-side-container">
+                  <div className="temperaments-detail">
+                    <h2>Types</h2>
+                    <p>{pokemonTypes}</p>
+                  </div>
+                  <hr />
+                  <div className="card-detail">
+                    <h2>Height</h2>
+                    <div>
+                      <span>Height</span>
+                      <span>{height || "No detail"}</span>
+                      <span>|</span>
+                      <span>Weight:</span>
+                      <span>{weight || "No detail"}</span>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="card-detail">
+                    <h2>Weight</h2>
+                    <div>
+                      <span>Hp:</span>
+                      <span>{hp || "No detail"}</span>
+                      <span>|</span>
+                      <span>Speed:</span>
+                      <span>{speed || "No detail"}</span>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="card-detail">
+                    <h2>Life Span</h2>
+                    <div>
+                      <span>Defense:</span>
+                      <span>{defense || "No detail"}</span>
+                      <span>|</span>
+                      <span>Force:</span>
+                      <span>{force || "No detail"}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+};
+
+export default CardDetails;
--- client/src/routes/details/Details.js
+++ client/src/routes/details/Details.js
@@ -5,17 +5,38 @@ import { connect } from "react-redux";
 import fetchPokemonById from "../../redux/reducers/fetchPokemonById";
 
 
+import CardDetails from "../../components/cardDetails/CardDetails";
 import "./details.css";
 
-// pokemonById => pokemonById.data[0].name
 const Details = ({ fetchPokemonById, pokemonById }) => {
-  const { id } = useParams();
-  
+  const { id }  = useParams();
+  const pokemon = pokemonById?.data[0];
+
   useEffect(() => {
     fetchPokemonById(id);
   }, [fetchPokemonById, id]);
 
-  return <h1>hello {pokemonById && pokemonById.data[0].name}</h1>
+
+  return (
+    <div className="detail--container">
+    {
+      pokemon 
+        ? <CardDetails
+            id={pokemon.id}
+            name={pokemon.name}
+            types={pokemon.types} 
+            hp={pokemon.hp}
+            force={pokemon.baseExperience}
+            speed={pokemon.speed}
+            image={pokemon.image}
+            height={pokemon.height}
+            weight={pokemon.weight}
+            defense={pokemon.defense}
+          />
+        : <h1>Loading...</h1>
+    }
+    </div>
+  ); 
 };
 
 
@@ -28,3 +49,8 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Details);
+
+
+/*
+      : <h1>Loading...</h1>
+ * */
