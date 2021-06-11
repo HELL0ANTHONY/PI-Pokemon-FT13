a7186d0af804884653419bee7177331ae79b595c
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-06-10 21:24:22 -0300

    Modal and useModal

---
+++ client/src/components/modal/Modal.js
@@ -0,0 +1,28 @@
+import { handleCloseClick } from "./ModalLogic";
+import Button from "../button/Button";
+import "./modal.css";
+
+const Modal = ({ children, isOpen, closeModal }) => {
+  return (
+    <div
+      className={`modal ${isOpen && "is-open"}`}
+      onClick={closeModal}
+    >
+      <div
+        className="modal-container"
+        onClick={handleCloseClick}
+      >
+        <div className="modal-close">
+          <Button
+            buttonStyle="btn--danger--solid"
+            buttonSize="btn--medium"
+            onClick={closeModal}
+          >close</Button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
