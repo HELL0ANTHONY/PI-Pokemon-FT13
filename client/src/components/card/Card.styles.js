de5ce8d2e3258ddc83b585aa30cb91fa33c5b007
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-06-15 23:57:47 -0300

    card alomost ready

--- client/src/components/card/Card.styles.js
+++ client/src/components/card/Card.styles.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-const colors = {};
+const colors = {
+  front: "#F9E79F",
+  back: "#2e2956",
+  orange: "#ff6b00",
+  cinc: "#fafbfd"
+};
 
 const flex = (align, justify, direction = "row") => {
   return `
@@ -38,17 +43,17 @@ export const CardContainer = styled.div`
 export const Front = styled.section`
   backface-visibility: hidden;
   ${flex("center", "flex-start", "column")};
-  ${commonStyles("100%", "100%", "8px", "absolute")};
-
-  background: #ffc728;
-  color: white;
+  ${commonStyles("100%", "100%", "8px", "absolute")}; 
+  background: ${colors.front};
+  -webkit-box-shadow: 5px 5px 15px 5px rgba(0,0,0,0.62); 
+  box-shadow: 5px 5px 15px 5px rgba(0,0,0,0.62);
 
   h1 {
     font-family: "Baloo Tammudu";
     font-weight: 700;
     font-style: italic;
     font-size: 30px;
-    color: red;
+    color: ${colors.back};
     margin-top: 20px;
   }
 `;
@@ -57,9 +62,9 @@ export const Box = styled.div`
   ${flex("center", "center", "column")};
   ${commonStyles("150px", "150px", "50%")};
   margin-top: 50px;
-  border-bottom: 10px solid red;
+  border-bottom: 10px solid ${colors.orange};
 
-  background: blue;
+  background: ${colors.back};
 `;
 
 export const Image = styled.div`
@@ -73,26 +78,27 @@ export const Back = styled(Front)`
   ${flex("center", "center", "column")}; 
   font-family: "Roboto Mono";
 
-  color: black;
-  background: pink;
+  color: ${colors.cinc};
+  background: ${colors.back};
 
   h2 {
     font-family: "Baloo Tammudu";
   }
 
   a {
+    color: ${colors.cinc};
     display: inline-block;
     text-decoration: none;
     text-align: center;
     margin-top: 5px;
     margin-right: 40px;
-    border: 1px solid black;
+    border: 1px solid ${colors.cinc};
     padding: 7px;
     border-radius: 4px;
+    transition: background 1s ease;
     
     :hover {
-      background: red;
-      color: white;
+      background: ${colors.orange};
     }
   } 
 `;
@@ -106,7 +112,7 @@ export const Id = styled.div`
       height: 25px;
       margin-top: 10px;
       margin-left: 25px;
-      border-left: 8px solid orange;
+      border-left: 8px solid ${colors.orange};
       border-top-left-radius : 3px;
       border-bottom-left-radius: 3px;
     }
@@ -120,7 +126,6 @@ export const List = styled.ul`
     font-size: 20px;
     display: block;
     width: 100%;
-    margin: 5px;
     padding: 5px;
     margin-right: 50px;
     text-align: center;
